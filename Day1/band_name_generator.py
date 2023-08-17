d6ee117626235bf489dcf09bd58e6b78e58eed84
Anuraag Mishra <anuraag.mishra@fau.de>
2023-08-17 14:35:30 +0200

    Day 1 done

---
+++ Day1/band_name_generator.py
@@ -0,0 +1,9 @@
+
+welcome_message = "Welcome to the band name generator."
+print(welcome_message)
+question1 = "What's the name of the city you grew up in?\n"
+question2 = "What's your pet's name?\n"
+birth_place = input(question1)
+pet_name = input(question2)
+band_name = "Your new band name could be " + birth_place + " " + pet_name
+print(band_name)
