df6b65a5416d3b87647e922b20f15d6a1797ce63
mishraanuraagx <anuraagmishra.214@gmail.com>
2023-08-20 14:05:28 +0200

    Day7 done. Added a  readme file.

--- Day7/hangman.py
+++ Day7/hangman.py
@@ -1,36 +1,73 @@
 
 import random
+import os
+import hangman_art
 
-wordlist = ["sugarcane", "apple", "pineapple"]
+words = []
+
+def read_words_from_file():
+    with open("wordlist.txt", 'r') as f:
+        words = f.readlines()
+    print(words[1:10])
+    return words
+
+words = read_words_from_file()
 
 print("Welcome to the Hangman game.")
 
-
-health = 7
-word = random.choice(wordlist)
+# health 0 means game over = 7th mistake
+health = 6
+word = random.choice(words)
+word = word.replace('\n','')
+# to show all the position of alphabets that player has correctly guessed
 guess_list = ["_" for _ in range(len(word))]
-guess = " ".join(guess_list)
 
 char_guessed_list = []
 
-while (health > 0 and "_" in guess ):
-    print(f"Word: {guess}    Health: {health}")
-    char_guess = input("Guess a letter.\n")[0]
+def clear_screen():
+    # Call clear screen command based on the OS
+    os.system('cls' if os.name=='nt' else 'clear')
+    
+
+def print_status(curr_health):
+    # Show the user number of position left to guess and health
+    print(hangman_art.HANGMAN_LOGO)
+    print(hangman_art.HANGMAN_ART[curr_health])
+    print(" Current status :\n Word: " + " ".join(guess_list)  + f"  Health: {health}")
+    print(f"Letters Guessed : {sorted(char_guessed_list)}")
+        
+
+while (health > 0 and "_" in guess_list ):
+    clear_screen()
+    # The ASCII art is 0-6 index
+    print_status(health)
+
+    # ask the player to guess a letter
+    char_guess = input("Guess a letter. ")[0].lower()
+
+    # Only go throw the flow if the player has guessed the alphabet for the first time.
     if char_guess not in char_guessed_list:
         char_guessed_list.append(char_guess);
         if char_guess in word:
+            # adding health back if the player guess is correct, else the game gets very hard based on the word length
+            if health < 6: health += 1
+
+            # good guess
             print("Found one!")
             for i in range(len(word)):
                 if word[i] == char_guess:
                     guess_list[i] = char_guess
         else:
+            # wrong guess
             print("Bad guess. Try again.")
             health -= 1
+            
 
-    guess = " ".join(guess_list)
 
+clear_screen()
+print_status(health)
 if (health == 0):
-    print(f"You couldn't guess the word, the word was {word}")
+    print(f"\n\nYou couldn't guess the word, the word was '{word}'")
 else:
     print(
-        f"Perfect! You were able to guess the word: {word} with {health} health left.")
+        f"Perfect! You were able to guess the word: '{word}' with {health} health left.")
---
+++ README.md
@@ -0,0 +1,20 @@
+## How to Use
+### Day7- Hangman
+    Playing this Hangman game is easy: Simply download the repository to your computer and run `hangman.py`! The game comes with its own included word list, `wordlist.txt`, which contains a list of 58,000+ English words from which the game can draw.
+
+## Customization
+### Day7- Hangman
+-Want to specify your own list of words in place of the included `wordlist.txt`? Simply overwrite that file with a text file of your own of the same filename, with each word on a separate line. Compound words with spaces involved work, too (eg., ice cream).
+
+-You can also add new words to the included `wordlist.txt` file to be used in the game. Words can be added at any place in the file but, again, add one word per line.
+
+## Requirements
+- The current project uses modules like random, os....to be continued.
+
+## Who wrote this?
+The program was written by Anuraag Mishra (anuraagmishra.214@gmail.com) as a leisurely project. I was learning Python to improve and decided to apply it to a project of my own to get some experience and to have some fun. This was the result!
+## Acknowledgements
+### Day7 - Hangman
+- WordList was obtained from <https://github.com/Tom25/Hangman>
+- ASCII art from <https://gist.github.com/chrishorton/8510732aa9a80a03c829b09f12e20d9c>
+- ASCII logo from <https://ascii.co.uk/art/hangman>
